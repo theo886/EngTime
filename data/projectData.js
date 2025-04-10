c440ac3bcd287e56aafc4f2b1b19ccf690f94b2c
theo886 <theo886@gmail.com>
2025-04-09 23:59:56 -0700

    Update projectData.js

--- data/projectData.js
+++ data/projectData.js
@@ -18,7 +18,7 @@ const projectData = [
   },
   {
     "id": "CP000038",
-    "name": "Stld Changeover Costs",
+    "name": "Skid Changeover Costs",
     "code": "CP000038",
     "color": "#E74C3C"
   },
@@ -42,7 +42,7 @@ const projectData = [
   },
   {
     "id": "RD000026",
-    "name": "Sales Orders",
+    "name": "Sage Geosystems",
     "code": "RD000026",
     "color": "#1ABC9C"
   },
@@ -77,51 +77,57 @@ const projectData = [
     "color": "#8E44AD"
   },
   {
-    "id": "VO000008",
+    "id": "VQ000008",
     "name": "Water Sales Support",
-    "code": "VO000008",
+    "code": "VQ000008",
     "color": "#27AE60"
   },
   {
-    "id": "VO000009",
+    "id": "VQ000009",
     "name": "PX, Turbo, Pump, Support",
-    "code": "VO000009",
+    "code": "VQ000009",
     "color": "#F39C12"
   },
   {
-    "id": "VO000010",
-    "name": "IPD Evaluation, PX Cost Reduction",
-    "code": "VO000010",
+    "id": "VQ000010",
+    "name": "PX Part Reduction, PX Cost Reduction",
+    "code": "VQ000010",
     "color": "#2980B9"
   },
   {
-    "id": "VO000011",
+    "id": "VQ000011",
     "name": "HP pump improvements",
-    "code": "VO000011",
+    "code": "VQ000011",
     "color": "#C0392B"
   },
   {
-    "id": "VO000012",
-    "name": "PX Power Improvements",
-    "code": "VO000012",
+    "id": "VQ000012",
+    "name": "ICAR/ Product Improvements",
+    "code": "VQ000012",
     "color": "#7F8C8D"
   },
-  {
-    "id": "VO000013",
+  { 
+    "id": "VQ000013",
     "name": "PX Q500 Development",
-    "code": "VO000013",
+    "code": "VQ000013",
     "color": "#D2B4DE"
   },
   {
-    "id": "WD000007",
+    "id": "WI000004",
+    "name": "Aquabold Improvements",
+    "code": "WI000004",
+    "color": "#3498DB" // Suitable color
+  },
+  {
+    "id": "WI000007",
     "name": "PX Q400 COGS Reduction",
-    "code": "WD000007",
+    "code": "WI000007",
     "color": "#A3E4D7"
   },
   {
-    "id": "WD000009",
+    "id": "WI000009",
     "name": "Turbo Std 550 and 875",
-    "code": "WD000009",
+    "code": "WI000009",
     "color": "#F9E79F"
   }
 ]
